e8163ac10bf34b86dc80208f60069368c7b6f340
Pedro Pazello <pedropazello.rj@gmail.com>
2014-12-24 10:08:51 -0200

    first week exercises done

--- firstSteps.hs
+++ firstSteps.hs
@@ -22,6 +22,9 @@ n = a `div` length xs
           xs = [1,2,3,4,5]
 
 -- Exercise 2
+
+
+
 -- last xs = drop (length xs - 1) xs
 -- last [1..5] = drop (length [1,2,3,4,5] -1) [1..5]
 -- last[1..5] = drop 4 [1,2,3,4,5]
@@ -62,3 +65,139 @@ n = a `div` length xs
 -- last [1..5] = reverse [1,2,3,4,5] !! 4
 -- last [1..5] = [5,4,3,2,1] !! 4
 -- last [1..5] = 1 -- incorrect
+
+
+-- Exercise 3
+
+
+-- init xs = tail (reverse xs)
+-- init [1..5] = tail ( reverse [1,2,3,4,5])
+-- init [1..5] = tail [5,4,3,2,1]
+-- init [1..5] = [4,3,2,1] -- incorrect
+
+-- init xs = reverse (head (reverse xs))
+-- init [1..5] = reverse (head (reverse [1,2,3,4,5]))
+-- init [1..5] = reverse (head [5,4,3,2,1])
+-- init [1..5] = reverse 5 -- cause error/incorrect
+
+-- init xs = reverse (tail xs)
+-- init [1..5] = reverse (tail [1,2,3,4,5])
+-- init [1..5] = reverse [2,3,4,5]
+-- init [1..5] = [5,4,3,2] -- incorrect
+
+-- init xs = take (length xs) xs
+-- init [1..5] = take (length [1,2,3,4,5]) [1..5]
+-- init [1..5] = take 5 [1,2,3,4,5]
+-- init [1..5] = [1,2,3,4,5] -- incorrect
+
+-- init xs = reverse (tail (reverse xs))
+-- init [1..5] = reverse (tail (reverse [1,2,3,4,5]))
+-- init [1..5] = reverse (tail [5,4,3,2,1])
+-- init [1..5] = reverse [4,3,2,1]
+-- init [1..5] = [1,2,3,4] -- correct
+
+-- init xs = take (length xs - 1) (tail xs)
+-- init [1..5] = take (length [1,2,3,4,5] - 1) (tail [1,2,3,4,5])
+-- init [1..5] = take 4 [2,3,4,5]
+-- init [1..5] = [2,3,4,5] -- incorrect
+
+-- init xs = drop (length xs - 1) xs
+-- init [1..5] = drop (length [1,2,3,4,5] - 1) [1..5]
+-- init [1..5] = drop 5 [1,2,3,4,5]
+-- init [1..5] = [] -- incorrect
+
+-- double (double 2)
+-- = { applying the inner double }
+-- double (2 + 2)
+-- = { applying double }
+-- (2 + 2) + (2 + 2)
+-- = { applying the ﬁrst + }
+-- 4 + (2 + 2)
+-- = { applying the second + }
+-- 4+4
+-- = { applying + }
+-- 8
+
+-- Exercise 5
+-- sum [x]
+-- =   { applying sum }
+-- x + sum []
+-- =   { applying sum }
+-- x + 0
+-- =   { applying + }
+-- x
+
+-- Exercise 6
+
+-- product [] = 1
+-- product (x : xs) = x * product xs
+
+-- product [2, 3, 4]
+-- =   { applying product }
+-- 2 ∗ (product [3,4])
+-- =   { applying product }
+-- 2 ∗ (3 ∗ product [4])
+-- =   { applying product }
+-- 2 ∗ (3 ∗ (4 ∗ product [ ]))
+-- =   { applying product }
+-- 2 ∗ (3 ∗ (4 ∗ 1))
+-- =   { applying ∗ }
+-- 24
+
+-- Exercise 7
+
+qsort [] = []
+qsort (x : xs) = qsort larger ++ [x] ++ qsort smaller
+  where smaller = [a | a <- xs, a <= x]
+        larger  = [b | b <- xs, b > x]	
+-- correct
+
+{--
+
+qsort [] = []
+qsort (x : xs) = reverse (qsort smaller ++ [x] ++ qsort larger)
+  where smaller = [a | a <- xs, a < x]
+        larger  = [b | b <- xs, b > x]	
+-- incorrect
+
+
+qsort [] = []
+qsort (x : xs) = reverse (qsort smaller ++ [x] ++ qsort larger)
+  where x = minimum xs
+        smaller = [a | a <- xs, a <= x]
+        larger  = [b | b <- xs, b > x]
+-- Exception: Prelude.minimum: empty list
+
+
+qsort [] = []
+qsort (x : xs) = reverse (qsort smaller) ++ [x] ++ reverse (qsort larger)
+   where smaller = [a | a <- xs, a <= x]
+         larger  = [b | b <- xs, b > x]
+-- incorrect
+
+qsort [] = []
+qsort (x : xs) = qsort larger ++ [x] ++ qsort smaller
+   where larger  = [a | a <- xs, a > x || a == x]
+         smaller = [b | b <- xs, b < x]
+-- correct
+
+qsort [] = []
+qsort (x : xs) = qsort larger ++ [x] ++ qsort smaller
+   where smaller = [a | a <- xs, a < x]
+         larger  = [b | b <- xs, b > x]
+-- correct but in the exercise page is incorrect
+
+qsort [] = []
+qsort (x : xs) = reverse 
+                 (reverse (qsort smaller) ++ [x] ++ reverse (qsort larger))
+                 where smaller = [a | a <- xs, a <= x]
+                       larger  = [b | b <- xs, b > x]
+-- correct
+
+qsort [] = []
+qsort xs = x : qsort larger ++ qsort smaller
+  where x = maximum xs
+        smaller = [a | a <- xs, a < x]
+        larger  = [b | b <- xs, b >= x]
+-- infinity loop
+--}
